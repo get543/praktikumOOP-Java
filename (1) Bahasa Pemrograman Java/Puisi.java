26f6b73c5fbdca6654ba6be44dc9a3f1c2973ce9
get543 <fghaza.pratama@gmail.com>
2024-05-15 21:24:08 +0700

    fix indentation & remove empty files

--- (1) Bahasa Pemrograman Java/Puisi.java
+++ (1) Bahasa Pemrograman Java/Puisi.java
@@ -1,27 +1,26 @@
 public class Puisi
 
 {
+    public static void main(String[] args)
 
-     public static void main(String[] args)
+    {
 
-     {
+        /*
 
-         /*
+            Ini adalah potongan puisi yang berjudul
 
-             Ini adalah potongan puisi yang berjudul
+            Sajak Putih karya Chairil Anwar
 
-             Sajak Putih karya Chairil Anwar
+        */
 
-         */
+        System.out.println("Hidup dari hidupku, pintu terbuka");
 
-     System.out.println("Hidup dari hidupku, pintu terbuka");
+        System.out.println("Selama matamu bagiku menengadah");
 
-     System.out.println("Selama matamu bagiku menengadah");
+        System.out.println("Selama kau darah mengalir dari luka");
 
-     System.out.println("Selama kau darah mengalir dari luka");
+        System.out.println("Antara kita mati datang tidak membelah");
 
-     System.out.println("Antara kita mati datang tidak membelah");
-
-     }
+    }
 
 }
